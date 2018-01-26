911d82ef5018df7e749208a2a7aa6dc088c05d60
Guiyolond <31778894+Guiyolond@users.noreply.github.com>
2018-01-26 17:11:23 -0500

    2 Section
    Maquetación dos primeras secciones del home.
    NOTA: Boton pa' Skype.

--- wp-content/themes/MaestrosTheme/header.php
+++ wp-content/themes/MaestrosTheme/header.php
@@ -6,10 +6,12 @@
     <!-- bootstrap -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link rel="stylesheet" href="<?php bloginfo(stylesheet_url); ?>" >
+    <link href="https://fonts.googleapis.com/css?family=Oswald:600" rel="stylesheet">
+
   </head>
   <body>
 
-      <div class="container fixed-top">
+      <div class="container fixed-top pt-5">
         <nav class="navbar navbar-light">
           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -17,10 +19,10 @@
           <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
             <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
               <li class="nav-item">
-                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                <a class="nav-link text-white" href="#">Home <span class="sr-only">(current)</span></a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="#">Link</a>
+                <a class="nav-link text-white" href="#">Link</a>
               </li>
             </ul>
           </div>
--- wp-content/themes/MaestrosTheme/index.php
+++ wp-content/themes/MaestrosTheme/index.php
@@ -1,34 +1,38 @@
 <?php get_header(); ?>
-<!--Elementos Home -->
-  <section id="home" class="container-fluid">
-    <div class="d-flex justify-content-center">
-      <div class="row">
-        <div class="col-12">
-          logo
-        </div>
-        <div class="col-12">
-          text
-        </div>
-        <div class="col-12">
-          fleha
-        </div>
-      </div>
-    </div>
-    <div class="row">
-      <div class="col-xl-">
-        <h1>VEA SU HOROSCOPO SEMANAL</h1><br><span class="">Tenemos un nuevo horóscopo poderoso....</span>
-      </div>
-      <div class="col-xl-">
-        <img src=""> <br><span class="">La llamada no tiene ningún costo, su operador...</span>      
-      </div>
-    </div>
-  </section>
+        <!--Elementos Home -->
+        <section id="home" class="container-fluid pb-5 pt-5">
+            <div class="d-flex justify-content-center pt-5">
+                <img src="<?php bloginfo('template_url'); ?>/img/simbolo.png">
+            </div>
+            <div class="d-flex justify-content-center pt-4">
+                <p class="text-warning text-center">BIENVENIDO<br> A LO QUE NO PUEDES COMPRENDER</p>
+            </div>
+            <div class="d-flex justify-content-center pt-4 pb-5">
+                <img src="<?php bloginfo('template_url'); ?>/img/arrow.png">
+            </div>
+        </section>
 
-<!--  -->
-    <section id="home2" class="container-fluid">
+        <section class="bg-gray">
+            <div class="container">
+                <div class="row pt-3 pb-3">
+                    <div class="col text-center pt-3">
+                        <h4 class="text-white">VEA SU HORÓSCOPO PODEROSO SEMANAL GRATIS</h4>
+                        <p class="text-white">Tenemos un nuevo horóscopo Poderoso cada semana<br> preparado para usted por los Mejores Maestros Consagrados</p>
+                    </div>
+                    <div class="col text-center pt-3">
+                        <!-- Implementación Skype-->
+                        <img src="" height="50px" width="75px" alt="Boton llamadas Skype" class="rounded bg-success" >
+                        <p class="text-white">La llamada no tiene ningún costo, su operador<br> telefónico puede aplicar los cargos estipulados</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <!--  -->
+        <section id="home2" class="container-fluid">
 
 
 
-    </section>
+        </section>
 
 <?php get_footer(); ?>
