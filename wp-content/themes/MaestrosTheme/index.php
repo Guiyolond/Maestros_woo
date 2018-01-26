0770f10e9ac73a5d9fcad0ebbe322869aa72a637
Guiyolond <31778894+Guiyolond@users.noreply.github.com>
2018-01-26 14:09:43 -0500

    maquetación

--- wp-content/themes/MaestrosTheme/index.php
+++ wp-content/themes/MaestrosTheme/index.php
@@ -1,4 +1,5 @@
 <?php get_header(); ?>
+<!--Elementos Home -->
   <section id="home" class="container-fluid">
     <div class="d-flex justify-content-center">
       <div class="row">
@@ -13,8 +14,21 @@
         </div>
       </div>
     </div>
-
+    <div class="row">
+      <div class="col-xl-">
+        <h1>VEA SU HOROSCOPO SEMANAL</h1><br><span class="">Tenemos un nuevo horóscopo poderoso....</span>
+      </div>
+      <div class="col-xl-">
+        <img src=""> <br><span class="">La llamada no tiene ningún costo, su operador...</span>      
+      </div>
+    </div>
   </section>
 
+<!--  -->
+    <section id="home2" class="container-fluid">
+
+
+
+    </section>
 
 <?php get_footer(); ?>
