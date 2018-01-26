bb4c6123f3c20b371c45c8a894b61063d6c087f0
a-serna <alejo.tanatos@gmail.com>
2018-01-26 17:25:45 -0500

    -

--- wp-content/themes/Maestrostheme/functions.php
+++ wp-content/themes/Maestrostheme/functions.php
@@ -5,7 +5,6 @@
   }
   add_action( 'after_setup_theme', 'my_theme_setup' );
 
-
   remove_action( 'woocommerce_before_main_content', 'woocommerce_output_content_wrapper', 10 );
   remove_action( 'woocommerce_after_main_content', 'woocommerce_output_content_wrapper_end', 10 );
 
