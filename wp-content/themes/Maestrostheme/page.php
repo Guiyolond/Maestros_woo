997d81fa039b1dc01b67106163f4ea6ce1c9d9ff
a-serna <alejo.tanatos@gmail.com>
2018-02-01 18:46:40 -0500

    page loop

--- wp-content/themes/Maestrostheme/page.php
+++ wp-content/themes/Maestrostheme/page.php
@@ -1,1 +1,27 @@
 <?php woocommerce_content(); ?>
+
+<?php get_header(); ?>
+    <article id="" class="container">
+      <div class="row pt-5 pl-5 pr-5">
+      	<div class="col-sm-12">
+      		<?php
+      			if( have_posts() ) {
+      				while( have_posts() ) {
+      					the_post();
+      					get_template_part( 'template-parts/content-single', '' );
+      				}
+
+      			}
+      			else {
+      				get_template_part( 'template-parts/content', 'none' );
+      			}
+      		?>
+      	</div>
+      </div>
+      <div class="row p-5">
+        <div class="col-12">
+          <?php comments_template(); ?>
+        </div>
+      </div>
+    </article>
+<?php get_footer(); ?>
