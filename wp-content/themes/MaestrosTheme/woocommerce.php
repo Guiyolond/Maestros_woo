2b140209f491badcd82f77e930e79d2cbe6f4971
a-serna <alejo.tanatos@gmail.com>
2018-02-01 18:55:38 -0500

    organizando woocommerce

--- wp-content/themes/MaestrosTheme/woocommerce.php
+++ wp-content/themes/MaestrosTheme/woocommerce.php
@@ -2,20 +2,9 @@
     <article id="" class="container">
       <div class="row pt-5 pl-5 pr-5">
       	<div class="col-sm-12">
-      		<!-- <?php
-      			if( have_posts() ) {
-      				while( have_posts() ) {
-      					the_post();
-      					get_template_part( 'template-parts/content-single', '' );
-      				}
-
-      			}
-      			else {
-      				get_template_part( 'template-parts/content', 'none' );
-      			}
-      		?> -->
 
             <?php woocommerce_content(); ?>
+
       	</div>
       </div>
       <div class="row p-5">
