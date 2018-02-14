eebe23500157adf1f0c090f201dca6493201897e
a-serna <alejo.tanatos@gmail.com>
2018-02-13 20:23:04 -0500

    enqueue all scripts and css

--- wp-content/themes/Maestrostheme/footer.php
+++ wp-content/themes/Maestrostheme/footer.php
@@ -46,14 +46,8 @@
       </div>
     </div>
 
-
-
-
-
     <!-- skype -->
     <script src="https://swc.cdn.skype.com/sdk/v1/sdk.min.js"></script>
-    <!-- smooth -->
-    <!-- <script src="<?php bloginfo(stylesheet_directory); ?>/js/smooth-scroll.min.js"></script> -->
     <?php wp_footer(); ?>
     <script type="text/javascript">
       var scroll = new SmoothScroll('a[href*="#"]', {
@@ -67,9 +61,5 @@
       	after: function () {}
       });
     </script>
-    <!-- js bootstrap -->
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
   </body>
 </html>
--- wp-content/themes/Maestrostheme/functions.php
+++ wp-content/themes/Maestrostheme/functions.php
@@ -2,9 +2,15 @@
   add_theme_support( 'title-tag' );
   add_theme_support( 'post-thumbnails' );
   function MaestrosTheme_script_enqueue(){
+    wp_enqueue_style( 'style', get_stylesheet_uri() );
     wp_enqueue_style( 'maestros', get_template_directory_uri() . '/css/maestros.css' , array(), '1.0.0', 'all');
     wp_enqueue_style( 'shop', get_template_directory_uri() . '/css/shop.css' , array(), '1.0.0', 'all');
+    wp_enqueue_style( 'bootstrap', 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css' , array(), '4.0.0', 'all');
+    wp_enqueue_style( 'googlefont', 'https://fonts.googleapis.com/css?family=Oswald:600');
     wp_enqueue_script( 'smooth-scroll', get_template_directory_uri() . '/js/smooth-scroll.min.js' , array(), '12.1.5', true);
+    wp_enqueue_script( 'jquery', 'https://code.jquery.com/jquery-3.2.1.slim.min.js' , array(), '3.2.1', true);
+    wp_enqueue_script( 'popper', 'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js' , array(), '1.0.0', true);
+    wp_enqueue_script( 'bootstrap', 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js' , array(), '4.0.0', true);
   }
   add_action('wp_enqueue_scripts', 'MaestrosTheme_script_enqueue');
   function woo_setup() {
