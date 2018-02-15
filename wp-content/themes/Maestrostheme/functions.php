ba4b82c5b15d5e5f86a8b42926f3ec8bb27c7aaa
a-serna <alejo.tanatos@gmail.com>
2018-02-15 16:39:34 -0500

    orden de catalogos servicios

--- wp-content/themes/Maestrostheme/functions.php
+++ wp-content/themes/Maestrostheme/functions.php
@@ -47,9 +47,7 @@
      }
   }
 
-  /**
-  * Output the product sorting options.
-  */
+  // orden del catalogo
   function woocommerce_catalog_ordering() {
     if ( ! wc_get_loop_prop( 'is_paginated' ) || ! woocommerce_products_will_display() ) {
       return;
@@ -88,23 +86,5 @@
     ) );
   }
 
-
-
-  add_filter( 'woocommerce_get_catalog_ordering_args', 'custom_woocommerce_get_catalog_ordering_args' );
-function custom_woocommerce_get_catalog_ordering_args( $args ) {
-  $orderby_value = isset( $_GET['orderby'] ) ? woocommerce_clean( $_GET['orderby'] ) : apply_filters( 'woocommerce_default_catalog_orderby', get_option( 'woocommerce_default_catalog_orderby' ) );
-	if ( 'random_list' == $orderby_value ) {
-		$args['orderby'] = 'rand';
-		$args['order'] = '';
-		$args['meta_key'] = '';
-	}
-	return $args;
-}
-add_filter( 'woocommerce_default_catalog_orderby_options', 'custom_woocommerce_catalog_orderby' );
-add_filter( 'woocommerce_catalog_orderby', 'custom_woocommerce_catalog_orderby' );
-function custom_woocommerce_catalog_orderby( $sortby ) {
-	$sortby['random_list'] = 'Random';
-	return $sortby;
-}
 
 ?>
