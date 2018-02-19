81282ec468e26e4c3ffd99fce0691c2bb5027d43
a-serna <alejo.tanatos@gmail.com>
2018-02-19 14:57:16 -0500

    git ignore agregue uploads

---
+++ wp-content/uploads/wpo_wcpdf/fonts/dompdf_font_family_cache.php
@@ -0,0 +1,122 @@
+<?php return array (
+  'sans-serif' => 
+  array (
+    'normal' => 'D:\\Websites\\Maestros_woo/wp-content/uploads/wpo_wcpdf/fonts/Helvetica',
+    'bold' => 'D:\\Websites\\Maestros_woo/wp-content/uploads/wpo_wcpdf/fonts/Helvetica-Bold',
+    'italic' => 'D:\\Websites\\Maestros_woo/wp-content/uploads/wpo_wcpdf/fonts/Helvetica-Oblique',
+    'bold_italic' => 'D:\\Websites\\Maestros_woo/wp-content/uploads/wpo_wcpdf/fonts/Helvetica-BoldOblique',
+  ),
+  'times' => 
+  array (
+    'normal' => 'D:\\Websites\\Maestros_woo/wp-content/uploads/wpo_wcpdf/fonts/Times-Roman',
+    'bold' => 'D:\\Websites\\Maestros_woo/wp-content/uploads/wpo_wcpdf/fonts/Times-Bold',
+    'italic' => 'D:\\Websites\\Maestros_woo/wp-content/uploads/wpo_wcpdf/fonts/Times-Italic',
+    'bold_italic' => 'D:\\Websites\\Maestros_woo/wp-content/uploads/wpo_wcpdf/fonts/Times-BoldItalic',
+  ),
+  'times-roman' => 
+  array (
+    'normal' => 'D:\\Websites\\Maestros_woo/wp-content/uploads/wpo_wcpdf/fonts/Times-Roman',
+    'bold' => 'D:\\Websites\\Maestros_woo/wp-content/uploads/wpo_wcpdf/fonts/Times-Bold',
+    'italic' => 'D:\\Websites\\Maestros_woo/wp-content/uploads/wpo_wcpdf/fonts/Times-Italic',
+    'bold_italic' => 'D:\\Websites\\Maestros_woo/wp-content/uploads/wpo_wcpdf/fonts/Times-BoldItalic',
+  ),
+  'courier' => 
+  array (
+    'normal' => 'D:\\Websites\\Maestros_woo/wp-content/uploads/wpo_wcpdf/fonts/Courier',
+    'bold' => 'D:\\Websites\\Maestros_woo/wp-content/uploads/wpo_wcpdf/fonts/Courier-Bold',
+    'italic' => 'D:\\Websites\\Maestros_woo/wp-content/uploads/wpo_wcpdf/fonts/Courier-Oblique',
+    'bold_italic' => 'D:\\Websites\\Maestros_woo/wp-content/uploads/wpo_wcpdf/fonts/Courier-BoldOblique',
+  ),
+  'helvetica' => 
+  array (
+    'normal' => 'D:\\Websites\\Maestros_woo/wp-content/uploads/wpo_wcpdf/fonts/Helvetica',
+    'bold' => 'D:\\Websites\\Maestros_woo/wp-content/uploads/wpo_wcpdf/fonts/Helvetica-Bold',
+    'italic' => 'D:\\Websites\\Maestros_woo/wp-content/uploads/wpo_wcpdf/fonts/Helvetica-Oblique',
+    'bold_italic' => 'D:\\Websites\\Maestros_woo/wp-content/uploads/wpo_wcpdf/fonts/Helvetica-BoldOblique',
+  ),
+  'zapfdingbats' => 
+  array (
+    'normal' => 'D:\\Websites\\Maestros_woo/wp-content/uploads/wpo_wcpdf/fonts/ZapfDingbats',
+    'bold' => 'D:\\Websites\\Maestros_woo/wp-content/uploads/wpo_wcpdf/fonts/ZapfDingbats',
+    'italic' => 'D:\\Websites\\Maestros_woo/wp-content/uploads/wpo_wcpdf/fonts/ZapfDingbats',
+    'bold_italic' => 'D:\\Websites\\Maestros_woo/wp-content/uploads/wpo_wcpdf/fonts/ZapfDingbats',
+  ),
+  'symbol' => 
+  array (
+    'normal' => 'D:\\Websites\\Maestros_woo/wp-content/uploads/wpo_wcpdf/fonts/Symbol',
+    'bold' => 'D:\\Websites\\Maestros_woo/wp-content/uploads/wpo_wcpdf/fonts/Symbol',
+    'italic' => 'D:\\Websites\\Maestros_woo/wp-content/uploads/wpo_wcpdf/fonts/Symbol',
+    'bold_italic' => 'D:\\Websites\\Maestros_woo/wp-content/uploads/wpo_wcpdf/fonts/Symbol',
+  ),
+  'serif' => 
+  array (
+    'normal' => 'D:\\Websites\\Maestros_woo/wp-content/uploads/wpo_wcpdf/fonts/Times-Roman',
+    'bold' => 'D:\\Websites\\Maestros_woo/wp-content/uploads/wpo_wcpdf/fonts/Times-Bold',
+    'italic' => 'D:\\Websites\\Maestros_woo/wp-content/uploads/wpo_wcpdf/fonts/Times-Italic',
+    'bold_italic' => 'D:\\Websites\\Maestros_woo/wp-content/uploads/wpo_wcpdf/fonts/Times-BoldItalic',
+  ),
+  'monospace' => 
+  array (
+    'normal' => 'D:\\Websites\\Maestros_woo/wp-content/uploads/wpo_wcpdf/fonts/Courier',
+    'bold' => 'D:\\Websites\\Maestros_woo/wp-content/uploads/wpo_wcpdf/fonts/Courier-Bold',
+    'italic' => 'D:\\Websites\\Maestros_woo/wp-content/uploads/wpo_wcpdf/fonts/Courier-Oblique',
+    'bold_italic' => 'D:\\Websites\\Maestros_woo/wp-content/uploads/wpo_wcpdf/fonts/Courier-BoldOblique',
+  ),
+  'fixed' => 
+  array (
+    'normal' => 'D:\\Websites\\Maestros_woo/wp-content/uploads/wpo_wcpdf/fonts/Courier',
+    'bold' => 'D:\\Websites\\Maestros_woo/wp-content/uploads/wpo_wcpdf/fonts/Courier-Bold',
+    'italic' => 'D:\\Websites\\Maestros_woo/wp-content/uploads/wpo_wcpdf/fonts/Courier-Oblique',
+    'bold_italic' => 'D:\\Websites\\Maestros_woo/wp-content/uploads/wpo_wcpdf/fonts/Courier-BoldOblique',
+  ),
+  'dejavu sans' => 
+  array (
+    'bold' => 'D:\\Websites\\Maestros_woo/wp-content/uploads/wpo_wcpdf/fonts/DejaVuSans-Bold',
+    'bold_italic' => 'D:\\Websites\\Maestros_woo/wp-content/uploads/wpo_wcpdf/fonts/DejaVuSans-BoldOblique',
+    'italic' => 'D:\\Websites\\Maestros_woo/wp-content/uploads/wpo_wcpdf/fonts/DejaVuSans-Oblique',
+    'normal' => 'D:\\Websites\\Maestros_woo/wp-content/uploads/wpo_wcpdf/fonts/DejaVuSans',
+  ),
+  'dejavu sans mono' => 
+  array (
+    'bold' => 'D:\\Websites\\Maestros_woo/wp-content/uploads/wpo_wcpdf/fonts/DejaVuSansMono-Bold',
+    'bold_italic' => 'D:\\Websites\\Maestros_woo/wp-content/uploads/wpo_wcpdf/fonts/DejaVuSansMono-BoldOblique',
+    'italic' => 'D:\\Websites\\Maestros_woo/wp-content/uploads/wpo_wcpdf/fonts/DejaVuSansMono-Oblique',
+    'normal' => 'D:\\Websites\\Maestros_woo/wp-content/uploads/wpo_wcpdf/fonts/DejaVuSansMono',
+  ),
+  'dejavu serif' => 
+  array (
+    'bold' => 'D:\\Websites\\Maestros_woo/wp-content/uploads/wpo_wcpdf/fonts/DejaVuSerif-Bold',
+    'bold_italic' => 'D:\\Websites\\Maestros_woo/wp-content/uploads/wpo_wcpdf/fonts/DejaVuSerif-BoldItalic',
+    'italic' => 'D:\\Websites\\Maestros_woo/wp-content/uploads/wpo_wcpdf/fonts/DejaVuSerif-Italic',
+    'normal' => 'D:\\Websites\\Maestros_woo/wp-content/uploads/wpo_wcpdf/fonts/DejaVuSerif',
+  ),
+  'open sans' => 
+  array (
+    'normal' => 'D:\\Websites\\Maestros_woo/wp-content/uploads/wpo_wcpdf/fonts/OpenSans-Normal',
+    'bold' => 'D:\\Websites\\Maestros_woo/wp-content/uploads/wpo_wcpdf/fonts/OpenSans-Bold',
+    'italic' => 'D:\\Websites\\Maestros_woo/wp-content/uploads/wpo_wcpdf/fonts/OpenSans-Italic',
+    'bold_italic' => 'D:\\Websites\\Maestros_woo/wp-content/uploads/wpo_wcpdf/fonts/OpenSans-BoldItalic',
+  ),
+  'segoe' => 
+  array (
+    'normal' => 'D:\\Websites\\Maestros_woo/wp-content/uploads/wpo_wcpdf/fonts/Segoe-Normal',
+    'bold' => 'D:\\Websites\\Maestros_woo/wp-content/uploads/wpo_wcpdf/fonts/Segoe-Bold',
+    'italic' => 'D:\\Websites\\Maestros_woo/wp-content/uploads/wpo_wcpdf/fonts/Segoe-Italic',
+    'bold_italic' => 'D:\\Websites\\Maestros_woo/wp-content/uploads/wpo_wcpdf/fonts/Segoe-BoldItalic',
+  ),
+  'roboto slab' => 
+  array (
+    'normal' => 'D:\\Websites\\Maestros_woo/wp-content/uploads/wpo_wcpdf/fonts/RobotoSlab-Normal',
+    'bold' => 'D:\\Websites\\Maestros_woo/wp-content/uploads/wpo_wcpdf/fonts/RobotoSlab-Bold',
+    'italic' => 'D:\\Websites\\Maestros_woo/wp-content/uploads/wpo_wcpdf/fonts/RobotoSlab-Italic',
+    'bold_italic' => 'D:\\Websites\\Maestros_woo/wp-content/uploads/wpo_wcpdf/fonts/RobotoSlab-BoldItalic',
+  ),
+  'currencies' => 
+  array (
+    'normal' => 'D:\\Websites\\Maestros_woo/wp-content/uploads/wpo_wcpdf/fonts/currencies',
+    'bold' => 'D:\\Websites\\Maestros_woo/wp-content/uploads/wpo_wcpdf/fonts/currencies',
+    'italic' => 'D:\\Websites\\Maestros_woo/wp-content/uploads/wpo_wcpdf/fonts/currencies',
+    'bold_italic' => 'D:\\Websites\\Maestros_woo/wp-content/uploads/wpo_wcpdf/fonts/currencies',
+  ),
+);
+?>
